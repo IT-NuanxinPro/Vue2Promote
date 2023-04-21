91fb603079627c070bb54df229bd174201c423b5
Julius Faris <1569250882@qq.com>
2023-04-21 16:08:11 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 5200,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        },
+      }
+    },
+  }
 })
